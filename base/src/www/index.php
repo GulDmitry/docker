287a779ff0550ec6f74fdc0fb4c035b482f56823
GulDmitry <guldmitry@gmail.com>
2016-07-19 17:17:08 +0300

    Some useful comments.

--- base/src/www/index.php
+++ base/src/www/index.php
@@ -9,5 +9,16 @@
 
 //$mysqli = new mysqli(getenv('DB_PORT_3306_TCP_ADDR'), 'root', 'root', 'sugarcrm');
 
-var_dump(['xdebug', 'test']);
+var_dump(['xdebug', 'works']);
+
+$mysqli = mysqli_init();
+$mysqli->options(MYSQLI_INIT_COMMAND, 'SET AUTOCOMMIT = 0');
+$mysqli->options(MYSQLI_OPT_CONNECT_TIMEOUT, 5);
+
+if (!$mysqli->real_connect('172.17.0.2', 'root', 'root', 'testdb')) {
+    die('Error ' . mysqli_connect_errno() . ') ' . mysqli_connect_error());
+}
+var_dump('Db host info', $mysqli->host_info);
+$mysqli->close();
+
 phpinfo();
