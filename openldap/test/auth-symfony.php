06cbca53b93273b9abe8fb2610cf13f7fe293f67
GulDmitry <guldmitry@gmail.com>
2016-08-12 10:50:10 +0300

    Symfony Auth.

---
+++ openldap/test/auth-symfony.php
@@ -0,0 +1,81 @@
+<?php
+require_once 'vendor/autoload.php';
+
+use Symfony\Component\Security\Core\Authentication\AuthenticationProviderManager;
+use Symfony\Component\Security\Core\User\UserChecker;
+use Symfony\Component\Security\Core\Authentication\Provider\LdapBindAuthenticationProvider;
+use Symfony\Component\Ldap\Ldap;
+use Symfony\Component\Ldap\Adapter\ExtLdap\Adapter;
+use Symfony\Component\Security\Core\User\LdapUserProvider;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
+
+/*
+ As an authentication mechanism:
+
+ # app/config/security.yml
+security:
+    # ...
+
+firewalls:
+    api:
+        provider:  app_users
+        stateless: true
+        pattern:   ^/api
+        http_basic_ldap:
+            service: app.ldap
+            dn_string: "{username}@example"
+    backend:
+        provider: app_users
+        pattern:  ^/admin
+        logout:
+            path:   logout
+            target: login
+        form_login_ldap:
+            service: app.ldap
+            dn_string: CN={username},OU=Users,DC=example,DC=com
+            check_path: login_check
+            login_path: login
+ */
+
+$config = [
+    'host' => 'localhost',
+    'port' => 389,
+];
+$baseDn = 'dc=openldap,dc=com';
+
+$adapter = new Adapter($config);
+$adapter->getConnection()->setOption('PROTOCOL_VERSION', 3);
+
+$ldap = new Ldap($adapter);
+
+// To use full DN string as a login, replace filter parameter.
+// Use `cn` as uidKey, default is Active Directory specific.
+$userProvider = new LdapUserProvider($ldap, $baseDn, 'cn=admin,' . $baseDn, 'admin', [], 'cn');
+
+// Without the search DN string provider cannot perform search.
+//$userProvider = new LdapUserProvider($ldap, 'dc=openldap,dc=com', null, null, [], 'cn');
+
+$authProvider = new LdapBindAuthenticationProvider(
+    $userProvider,
+    new UserChecker(),
+    'ldap',
+    $ldap,
+    'cn={username},cn=users,' . $baseDn, // Bind DN string. Users in the `users` group (see README.md).
+    //'cn={username}' . $baseDn, // To log in as admin.
+    false
+);
+
+$authManager = new AuthenticationProviderManager([
+    $authProvider,
+]);
+
+// To use DN as login the provider should be tuned.
+//$unAuthToken = new UsernamePasswordToken('cn=admin,dc=openldap,dc=com', $password, 'ldap');
+//$unAuthToken = new UsernamePasswordToken('admin', 'admin', 'ldap');
+$unAuthToken = new UsernamePasswordToken('test', 'test', 'ldap');
+
+$token = $authManager->authenticate($unAuthToken);
+
+$result = $token->isAuthenticated();
+
+var_dump('Good!', $result);
